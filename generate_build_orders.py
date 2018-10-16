d2ea6bebbab01b591f3a03c5a5b0eda92244c25a
Steven Keyes <stevenrkeyes@gmail.com>
2018-10-15 21:20:54 -0700

    Added script to generate build orders
    This patch adds generate_build_orders.py, which generates build orders and (if called as the main program) saves them to a file.

---
+++ generate_build_orders.py
@@ -0,0 +1,54 @@
+# generates an inital generation of build orders
+# and saves it to a specified filename
+
+import sys
+import random
+import pickle
+
+import game_constants
+import towers
+import map_manager
+import play_build_order
+
+BUILD_ORDER_LENGTH = 20
+NUM_TO_GENERATE = 20
+
+def generate_build_order():
+	# make up a build order, using a map_manager to avoid invalid tower placements
+	mm = map_manager.MapManager("occupancy_grid.png")
+	build_order = []
+	for i in range(BUILD_ORDER_LENGTH):
+		#action_enum = random.randint(5 + 2*len(towers))
+		action_enum = random.randint(0,4)
+
+		tower_type = towers.TowerTypes(action_enum)
+		x = random.randint(0, mm.shape[1] - 1)
+		y = random.randint(game_constants.url_bar_height, mm.shape[0] - 1)
+		while mm.is_occupied((x,y)):
+			x = random.randint(0, mm.shape[1] - 1)
+			y = random.randint(game_constants.url_bar_height, mm.shape[0] - 1)
+		coords = (x, y)
+		mm.add_object(coords, game_constants.tower_interference_radius)
+		step = play_build_order.BuildOrderStep(tower_type, coords)
+		build_order.append(step)
+	return build_order
+
+def generate_build_orders(num_to_generate):
+	build_orders = []
+	for i in range(num_to_generate):
+		build_order = generate_build_order()
+		build_orders.append(build_order)
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print()
+		print("You must pass a name for the generation file. Example:")
+		print("   $ python generate_build_order.py gen_0")
+		print()
+		sys.exit()
+
+	build_orders = generate_build_orders(NUM_TO_GENERATE)
+
+	filename = sys.argv[1] + ".pkl"
+	with open(filename, 'wb') as f:
+		pickle.dump(build_orders, f, pickle.HIGHEST_PROTOCOL)
--- play.py
+++ play.py
@@ -3,58 +3,17 @@ import pickle
 
 import game_constants
 import towers
-
 import map_manager
 import play_build_order
+import generate_build_orders
 
-# make up a build order, using a map_manager to avoid invalid tower placements
-mm = map_manager.MapManager("occupancy_grid.png")
-build_order = []
-for i in range(10):
-	#action_enum = random.randint(5 + 2*len(towers))
-	action_enum = random.randint(0,4)
-	#switch based on action type
+build_order = generate_build_orders.generate_build_order()
 
-	tower_type = towers.TowerTypes(action_enum)
-	x = random.randint(0, mm.shape[1] - 1)
-	y = random.randint(game_constants.url_bar_height, mm.shape[0] - 1)
-	while mm.is_occupied((x,y)):
-		x = random.randint(0, mm.shape[1] - 1)
-		y = random.randint(game_constants.url_bar_height, mm.shape[0] - 1)
-	coords = (x, y)
-	mm.add_object(coords, game_constants.tower_interference_radius)
-	step = play_build_order.BuildOrderStep(tower_type, coords)
+for step in build_order:
 	print(step)
-	build_order.append(step)
 
 end_time, build_order_index, stats_list = play_build_order.play(build_order)
 
 with open('data.pkl', 'wb') as f:
 	data = (build_order, build_order_index, stats_list)
 	pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
-
-
-'''
-t = 0
-print("Build order:")
-for i in range(len(build_order)):
-	print('{:>10}'.format(round(t, 3)), "sec:", build_order[i])
-	t += build_order_delays[i]
-print(gm.get_stats())
-'''
-
-'''
-here's how we can generate a build order
- - pick a random time to do a step
- - when that time arrives, pick a random type of step (out of 5 + 2*n options, where n is the current number of towers)
-   - if we can't actually afford that option then choose another option
-   - if we can't actually afford any options, then skip
-   - if it's a tower, then pick a non-occupied place on the map, and add it to the occupancy grid
- - repeat
-'''
-
-'''
-get round
-get money
-get lives
-'''
